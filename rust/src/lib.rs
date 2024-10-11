522e13849e0769c14e4af6d89950bad0349126bd
ItzBreakfast <53260215+ItzBreakfast@users.noreply.github.com>
2024-10-11 22:24:18 +0900

    Added future code refactoring plans.

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -2,6 +2,10 @@
 
 use godot::prelude::*;
 
+// TODO: Refactory the code (player.rs above all) with following methods:
+//         - Add an animation state variable to avoid .into() abuse.
+//         - Put out some conditions from if statements using variable as category.
+
 mod camera;
 mod collider;
 mod enemy;
